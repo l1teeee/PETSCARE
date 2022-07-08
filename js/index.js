1d45027c71708e3a20ac340d1db4e70eb9523bc3
l1teeee <alejandro23.am29@gmail.com>
2022-07-07 18:52:47 -0600

    Primera

---
+++ js/index.js
@@ -0,0 +1,22 @@
+window.sr = ScrollReveal();
+
+    sr.reveal('.sup', {
+        duration: 3000,
+        origin: 'bottom',
+        distance:'-100px'
+
+    });
+
+    sr.reveal('.scroll-banner', {
+        duration: 5000,
+        origin: 'right',
+        distance:'-50px'
+
+    });
+
+    sr.reveal('.rounded', {
+        duration: 5000,
+        origin: 'left',
+        distance:'-70px'
+
+    });
